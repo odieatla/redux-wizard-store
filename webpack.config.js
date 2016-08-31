6a11de57ef97a0ae7b16e14b1dd7e3558dbd9bed
Ouning Zhang <ouning.zhang@gmail.com>
2016-08-30 18:19:56 -0700

    added scss loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var path = require('path');
 var webpack = require('webpack');
+var extractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'source-map',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
@@ -10,17 +11,22 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].js',
     publicPath: '/static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['react-hot', 'babel'],
       include: path.join(__dirname, 'src')
+    }, {
+      test: /\.scss$/,
+      loader: extractTextPlugin.extract('style', 'css?sourceMap!sass?sourceMap')
+      // loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
     }]
-  }
+  },
+  plugins: [
+    new extractTextPlugin('styles.css'),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
